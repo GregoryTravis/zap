09c40ddcc1fa0fedcf5facce4a6c22cc1f14c9e9
Gregory Travis <greg.m.travis@gmail.com>
2021-08-23 19:38:43 -0400

    a few physics steps

--- src/fiz/mod.rs
+++ src/fiz/mod.rs
@@ -7,7 +7,7 @@ use nphysics3d::force_generator::DefaultForceGeneratorSet;
 use nphysics3d::joint::DefaultJointConstraintSet;
 use nphysics3d::world::{DefaultMechanicalWorld, DefaultGeometricalWorld};
 use nphysics3d::object::{RigidBodyDesc};
-// use nphysics3d::math::{Velocity, Inertia};
+use nphysics3d::math::{Velocity};
 use ncollide3d::shape::{ShapeHandle, Ball};
 // use ncollide3d::world::CollisionGroups;
 use nphysics3d::object::ColliderDesc;
@@ -21,6 +21,9 @@ pub fn fiz_main() {
     let rigid_body = RigidBodyDesc::new()
         .translation(Vector3::y() * 5.0)
         .mass(1.2)
+        .gravity_enabled(true)
+        // .set_status(BodyStatus::Kinematic)
+        .velocity(Velocity::linear(1.0, 2.0, 3.0))
         .build();
 
     let mut bodies = DefaultBodySet::new();
@@ -37,7 +40,8 @@ pub fn fiz_main() {
     let mut joint_constraints = DefaultJointConstraintSet::new();
     let mut force_generators = DefaultForceGeneratorSet::new();
 
-    loop {
+    // loop {
+    for i in 0..4 {
         // println!("ooo");
         // Run the simulation.
         mechanical_world.step(
@@ -47,6 +51,11 @@ pub fn fiz_main() {
             &mut joint_constraints,
             &mut force_generators
         );
-        return;
+        let body = bodies.rigid_body(handle).unwrap();
+        let x = body.position().translation.x;
+        println!("oy {}", body.position());
+        println!("oy2 {}", mechanical_world.timestep());
+// let platform = bodies.rigid_body_mut(platform_handle).unwrap();
+//         let platform_z = platform.position().translation.z;
     }
 }
